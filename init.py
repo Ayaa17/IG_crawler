40f1087987ee1ec6cfd7421f9a2d98f897dc05eb
Kent@DESKTOP-92HDSMQ <[kent831217@gmail.com]>
2021-10-02 14:41:23 +0800

    1002

---
+++ init.py
@@ -0,0 +1,28 @@
+
+# 網址
+url_main = 'https://www.instagram.com/'
+url_solar = 'https://www.instagram.com/solarkeem/'
+url_moon ='https://www.instagram.com/mo_onbyul/'
+url_whee = 'https://www.instagram.com/whee_inthemood/'
+url_hwasa ='https://www.instagram.com/_mariahwasa/'
+url_mamamoo = "https://www.instagram.com/mamamoo_official/"
+
+
+
+url_prefix_post ="https://www.instagram.com/p/"
+
+# webdriver 參數
+implicitly_wait_time = 5
+
+# 定位用參數
+elem_userText = "username"
+elem_passwordText = "password"
+elem_submit = 'sqdOP.L3NKy.y3zKF'
+
+pic_loc = "eLAPa"
+elem_showMore = "tCibT.qq7_A.z4xUb.w5S7h"
+
+uri_query = 'https://www.instagram.com/graphql/query/?query_hash=a5164aed103f24b03e7b7747a2d94e3c&variables=%7B%22id%22%3A%22{user_id}%22%2C%22first%22%3A12%2C%22after%22%3A%22{cursor}%22%7D'
+
+uri_query2='https://www.instagram.com/graphql/query/?query_hash=d4d88dc1500312af6f937f7b804c68c3&variables=%7B%22user_id%22%3A%226928069064%22%2C%22include_chaining%22%3Atrue%2C%22include_reel%22%3Atrue%2C%22include_suggested_users%22%3Afalse%2C%22include_logged_out_extras%22%3Afalse%2C%22include_highlight_reels%22%3Atrue%2C%22include_live_status%22%3Atrue%7D'
+uri_query3='https://www.instagram.com/graphql/query/?query_hash=8c2a529969ee035a5063f2fc8602a0fd&variables=%7B%22id%22%3A%22{user_id}%22%2C%22first%22%3A12%2C%22after%22%3A%22{cursor}%22%7D'
---
+++ main.py
@@ -0,0 +1,104 @@
+import json
+import os
+import sqlite3
+import time
+from seleniumrequests import Chrome
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup as Soup
+import requests
+import database
+import downloadpost
+import getindex
+import getinfo
+
+# 首頁
+import getstroies
+import init
+
+
+print("Enter account: ")
+account = input()
+print("Enter password:　")
+password = input()
+# account
+# password
+
+
+url_main = 'https://www.instagram.com/'
+browser = webdriver.Chrome()
+browser.get(url_main)
+
+# 等待100s 以讀取網頁
+browser.implicitly_wait(100)
+# 登入
+islogin = True
+
+elem_user = browser.find_element_by_name("username").send_keys(account)
+elem_password = browser.find_element_by_name("password").send_keys(password)
+elem_submit = browser.find_element_by_class_name('sqdOP.L3NKy.y3zKF').click()
+time.sleep(10)
+
+database_name = 'Instagram.db'
+# 取得資料/目標網址
+username = '''mamamoo_official'''
+url_main = init.url_main
+url_Target = init.url_main + username + '/'
+
+# 取得資料Post
+result = getinfo.main(browser, url_Target, islogin, database_name, username)
+# user_id=267920419
+# cursor='QVFBeGtwREpmNEgzZnVrOHB4LTlVa0xmM2NEbW1paGdaT3c5SEE2XzV1TVJxOFl2ckFmcXFiTDl2dGxZODVxVlZybG5mNHdESWx5bmFsb2F1OGpEMlFJQg=='
+# uri_page2 = init.uri_query.format(user_id=user_id, cursor=cursor)
+# result = getinfo.main_continue(browser, uri_page2, islogin,cursor, user_id, database_name,username)
+# print("DB all over")
+
+# 下載Post
+# downloadpost.main(islogin, browser, database_name, username)
+
+# 限時動態
+# getstroies.main(browser, username, islogin)
+
+# 更新POST LIST
+# getinfo.refresh(browser, url_Target, islogin, database_name, username)
+
+
+# 輪詢更新 get user info + 限時
+# browser.implicitly_wait(5)
+# users_list = database.getTablename(database_name)
+# for i in users_list:
+#     print(i[0]+"...", end='')
+#     user_name = i[0]
+#     url_main = init.url_main
+#     url_Target = init.url_main + user_name + '/'
+#     # getindex.main(islogin, browser, url_Target)
+#     try:
+#         getindex.main(islogin, browser, url_Target)
+#         time.sleep(1)
+#         try:
+#             print(user_name +"get stories...",end="")
+#             getstroies.main(browser, user_name, islogin)
+#             print("OVER!!")
+#         except:
+#             print("FAIL!!")
+#
+#     except:
+#         print(i[0] + "is fail")
+
+
+
+
+# 輪詢更新POST LIST
+# users_list = database.getTablename(database_name)
+# for i in users_list:
+#     print(i[0] + "...", end='')
+#     user_name = i[0]
+#     url_main = init.url_main
+#     url_Target = init.url_main + user_name + '/'
+#     try:
+#         getinfo.refresh(browser, url_Target, islogin, database_name, user_name)
+#         print("refresh OK!!:" )
+#     except:
+#         print("refresh FAIL!! :")
